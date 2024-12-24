31310a03acec6f243f3c717f64667e6a0bf4f39a
Bilal Marbouh <wsking539@gmail.com>
2024-12-24 10:39:19 +0100

    Add files via upload

---
+++ ps22.js
@@ -0,0 +1,38 @@
+function inArray(array1,array2){
+    let newArr=[];
+    for(let i=0;i<array1.length;i++){
+        for(let g=0;g<array2.length;g++){
+            if(array2[g].includes(array1[i])){
+                newArr.push(array1[i])
+            }
+
+        }
+    }
+    return [...new Set(newArr)].sort();
+}
+
+// Question:
+// Given two arrays of strings a1 and a2 return a
+// sorted array r in lexicographical order of the
+// strings of a1 which are substrings of strings of a2.
+
+// Example 1:
+// a1 = ["arp", "live", "strong"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns ["arp", "live", "strong"]
+
+// Example 2:
+// a1 = ["tarp", "mice", "bull"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns []
+
+// Notes:
+// Arrays are written in "general" notation. See "Your Test Cases"
+// for examples in your language.
+// In Shell bash a1 and a2 are strings. The return is a string where
+// words are separated by commas.
+// Beware: In some languages r must be without duplicates.
